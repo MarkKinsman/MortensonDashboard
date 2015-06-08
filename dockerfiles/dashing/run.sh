a5299401263050d51078286597416991cab1d0ae
MarkKinsman <mark@kinsman.net>
2015-06-07 18:31:20 -0700

    Add Run.sh

---
+++ dockerfiles/dashing/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function install_widgets() {
+  WIDGETS=$@
+  if [[ ! -z "$WIDGETS" ]]; then
+    for WIDGET in $WIDGETS; do
+      echo -e "\nInstalling widget from gist $WIDGET"
+      dashing install "$WIDGET"
+    done
+  fi
+}
+
+function install_gems() {
+  GEMS=$@
+  if [[ ! -z "$GEMS" ]]; then
+    echo -e "\nInstalling gem(s): $GEMS"
+    for GEM in $GEMS; do
+      echo -e "\ngem '$GEM'" >> Gemfile
+    done
+    bundle install
+  fi
+}
+
+if [[ ! -e /installed ]]; then
+  install_widgets $WIDGETS
+  install_gems $GEMS
+  touch /installed
+fi
+
+if [[ ! -z "$PORT" ]]; then
+  PORT_ARG="-p $PORT"
+fi
+
+# Append to the end of the file
+echo "export TERM=xterm" >> ~/.bash_profile
+
+# Start dashing
+exec dashing start $PORT_ARG
