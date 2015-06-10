95e93e2b7f511955d52bc0b8f162b14db2aad5f6
MarkKinsman <mark@kinsman.net>
2015-06-10 16:43:24 -0700

    Leaderboard

--- dockerfiles/dashing/jobs/punchlist.rb
+++ dockerfiles/dashing/jobs/punchlist.rb
@@ -5,6 +5,7 @@ username=0
 password=0
 project=0
 widgets=['company_1','company_2','company_3','company_4','company_5','company_6','company_7','company_8','company_9','company_10','company_11','company_12']
+leaders = Hash.new()
 count=0
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
@@ -13,7 +14,12 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
 
 
   widgets.each do |e|
-    send_event(e, {title: e, open: count, ready: rand(100), complete: rand(200), closed: rand(300)})
+    r = rand(100)
+    c = rand(200)
+    f = rand(300)
+    send_event(e, {title: e, open: count, ready: r, complete: c, closed: f)})
+    leaders[e] = f/count
     count += 1
   end
+  send_event('leaderboard', {items: leaders.values})
 end
