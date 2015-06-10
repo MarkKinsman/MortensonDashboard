f0a3b51e272a9fa48d665d6e89b1acc0d770a89e
MarkKinsman <mark@kinsman.net>
2015-06-10 10:51:25 -0700

    Debug

--- dockerfiles/dashing/jobs/punchlist.rb
+++ dockerfiles/dashing/jobs/punchlist.rb
@@ -1,14 +1,14 @@
-require 'rest-client'
-require 'json'
+#require 'rest-client'
+#require 'json'
 
-username=0
-password=0
-project=0
-widgets=['company1','company2','company3','company4','company5','company6','company7','company8','company9','company10','company11','company12']
-count=0
+#username=0
+#password=0
+#project=0
+#widgets=['company1','company2','company3','company4','company5','company6','company7','company8','company9','company10','company11','company12']
+#count=0
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '1s', :first_in => 0 do |job|
+SCHEDULER.every '1m', :first_in => 0 do |job|
 
 
 
