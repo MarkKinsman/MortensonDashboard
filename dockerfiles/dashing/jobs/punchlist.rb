aa42feef3c9a6a91a3c06003aeba8ada45e70fc9
MarkKinsman <mark@kinsman.net>
2015-06-11 13:29:57 -0700

    Debug

--- dockerfiles/dashing/jobs/punchlist.rb
+++ dockerfiles/dashing/jobs/punchlist.rb
@@ -19,7 +19,7 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     c = rand(200)
     f = rand(300)
     send_event(e, {title: e, open: count, ready: r, complete: c, closed: f})
-    leaders[e] = {label: e, value: ( (f / (o + r + c + f )*100)}
+    leaders[e] = {label: e, value: ( (f / (o + r + c + f )) *100)}
   end
   send_event('leaderboard', { items: leaders.values})
 end
