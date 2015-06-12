74e3519cef0059c483c5848413c745f34d413b3f
MarkKinsman <mark@kinsman.net>
2015-06-12 10:31:42 -0700

    Call fixed

--- dockerfiles/dashing/jobs/punchlist.rb
+++ dockerfiles/dashing/jobs/punchlist.rb
@@ -5,7 +5,7 @@ username=0
 password=0
 project=0
 widgets=['company_0','company_1','company_2','company_3','company_4','company_5','company_6','company_7','company_8','company_9','company_10','company_11']
-base_url = "http://bim360.autodesk.com/"
+base_url = "http://bim360field.autodesk.com/"
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
@@ -27,7 +27,7 @@ begin
 rescue => err
   send_event('debug', {text: err.response})
 else
-  send_event('debug', {text: ticket.response})
+  send_event('debug', {text: ticket.response + ticket.body})
 end
 
   widgets.each do |e|
