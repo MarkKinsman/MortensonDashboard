3fe3d9af15a9f1515b8fa4a529af32416858e668
MarkKinsman <mark@kinsman.net>
2015-06-11 18:33:17 -0700

    Debug

--- dockerfiles/dashing/jobs/punchlist.rb
+++ dockerfiles/dashing/jobs/punchlist.rb
@@ -4,7 +4,7 @@
 username=0
 password=0
 project=0
-widgets=['company_1','company_2','company_3','company_4','company_5','company_6','company_7','company_8','company_9','company_10','company_11','company_12']
+widgets=['company_0','company_1','company_2','company_3','company_4','company_5','company_6','company_7','company_8','company_9','company_10','company_11']
 count=1
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
@@ -18,7 +18,7 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     r = rand(20)
     c = rand(20)
     f = rand(100)
-    companies[e] = {title: e, open: o, ready: r, complete: c, closed: f}
+    companies[e] = {title: e, open: o, ready: r, complete: c, closed: f, total: o + r + c + f }
     value = (f*100) / (o + r + c + f )
     leaders[e] = {label: e, value: "#{value}%"}
   end
