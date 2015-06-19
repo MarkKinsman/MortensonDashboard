37c2fb88cc1275b8a5b3377f079ed0a4e9a8f392
MarkKinsman <mark@kinsman.net>
2015-06-19 14:41:16 -0700

    Error Check

--- dockerfiles/dashing/jobs/punchlist.rb
+++ dockerfiles/dashing/jobs/punchlist.rb
@@ -13,7 +13,6 @@ SCHEDULER.every '1m', :first_in => 0, allow_overlapping: false do |job|
   project_ticket=0
   leaders = Hash.new({value: 0})
   companies = Hash.new({name: 0, open: 0, complete: 0, ready: 0, closed: 0, total: 0})
-  total = Hash.new({name: 0, open: 0, complete: 0, ready: 0, closed: 0, total: 0})
 
   begin
     File.open(File.expand_path("../login", __FILE__ ), "r") do |rf|
@@ -22,62 +21,57 @@ SCHEDULER.every '1m', :first_in => 0, allow_overlapping: false do |job|
         project = rf.readline.chomp
     end
   rescue
-    send_event('debug', {text: $!})
-  end
-
-  stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/login", :params => {:username => username, :password => password}))
-  login_ticket = stream["ticket"]
-  stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/projects", :params => {:ticket => login_ticket}))
-  stream.each do |projects|
-    if projects["name"] == project
-      project_ticket = projects["project_id"]
-    end
-  end
-
-  stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/companies/", :params => {:ticket => login_ticket, :project_id => project_ticket}))
-  stream.each do |c|
-    companies[c["company_id"]] = {name: c["name"], open: 0, ready: 0, complete: 0, closed: 0, total: 0}
+    send_event('debug', {text: "Open: " << $!})
   end
 
   begin
-    stream = JSON.parse(RestClient::Request.execute(method: :get, url: "http://bim360field.autodesk.com/api/get_issues/", timeout: nil, headers: {:params => {:ticket => login_ticket, :project_id => project_ticket}}))
-  rescue
-    send_event('debug', {text: $!})
-  end
-
-  stream.each do |i|
-    if i["issue_type"].include? "Punch List"
-      case i["status"]
-        when "Open" then companies[i["company_id"]][:open] += 1
-        when "Work Completed" then companies[i["company_id"]][:complete] += 1
-        when "Ready to Inspect" then companies[i["company_id"]][:ready] += 1
-        when "Closed" then companies[i["company_id"]][:closed] += 1
+    stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/login", :params => {:username => username, :password => password}))
+    login_ticket = stream["ticket"]
+    stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/projects", :params => {:ticket => login_ticket}))
+    stream.each do |p|
+      if p["name"] == project
+        project_ticket = p["project_id"]
       end
-      companies[i["company_id"]][:total] += 1
     end
+    stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/companies/", :params => {:ticket => login_ticket, :project_id => project_ticket}))
+    stream.each do |c|
+      companies[c["company_id"]] = {name: c["name"], open: 0, ready: 0, complete: 0, closed: 0, total: 0}
+    end
+  rescue
+    send_event('debug', {text: "Companies: " << $!})
+  end
+  
+  begin
+    stream = JSON.parse(RestClient::Request.execute(method: :get, url: "http://bim360field.autodesk.com/api/get_issues/", timeout: nil, headers: {:params => {:ticket => login_ticket, :project_id => project_ticket}}))
+    send_event('debug', {text: stream})
+  rescue
+    send_event('debug', {text: "Issues: " << $!})
   end
 
-  send_events('debug', {text: stream})
-
-  companies_array = companies.sort_by { |k, v| v[:open] }.reverse!
-
-#begin
-#  companies.each do |k, v|
-#    value = v[:closed] * 100 / v[:total]
-#    leaders[v[:name]] = {label: v[:name], value: "#{value}%"}
-#    total[:open] += v[:open]
-#    total[:complete] += v[:complete]
-#    total[:ready] += v[:ready]
-#    total[:closed] += v[:closed]
-#
-#  end
-#rescue
-#  send_events('debug', {text: $!})
-#end
-
-  12.times do |i|
-    send_event(widgets[i], {title: companies_array[i][1][:name], open: companies_array[i][1][:open], ready: companies_array[i][1][:ready], complete: companies_array[i][1][:complete], closed: companies_array[i][1][:closed] })
+  begin
+    stream.each do |i|
+      if i["issue_type"].include? "Punch List"
+        case i["status"]
+          when "Open" then companies[i["company_id"]][:open] += 1
+          when "Work Completed" then companies[i["company_id"]][:complete] += 1
+          when "Ready to Inspect" then companies[i["company_id"]][:ready] += 1
+          when "Closed" then companies[i["company_id"]][:closed] += 1
+        end
+        companies[i["company_id"]][:total] += 1
+      end
+    end
+  rescue
+    send_event('debug', {text: "Count Issues: " << $!})
   end
-#  send_event('total', {title: "Total", open: total[:open], closed: total[:closed], ready: total[:ready], complete: total[:complete]})
-  send_event('leaderboard', { items: leaders.values })
+
+  begin
+    companies_array = companies.sort_by { |k, v| v[:open] }.reverse!
+    12.times do |i|
+      send_event(widgets[i], {title: companies_array[i][1][:name], open: companies_array[i][1][:open], ready: companies_array[i][1][:ready], complete: companies_array[i][1][:complete], closed: companies_array[i][1][:closed] })
+    end
+    send_event('total', {title: "Total", open: total[:open], closed: total[:closed], ready: total[:ready], complete: total[:complete]})
+    send_event('leaderboard', { items: leaders.values })
+  rescue
+    send_event('debug', {text: "Display: " << $!})
+  end
 end
