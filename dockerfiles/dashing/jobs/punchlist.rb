44bb621de634adf2d9c7871a8503e0d89d8d8201
MarkKinsman <Mark@kinsman.net>
2015-06-23 16:05:00 +0000

    Ready for prod

--- dockerfiles/dashing/jobs/punchlist.rb
+++ dockerfiles/dashing/jobs/punchlist.rb
@@ -89,7 +89,7 @@ SCHEDULER.every '1m', :first_in => 0, allow_overlapping: false do |job|
       if v[:total] != 0
 	      send_event('debug', {text: debug_console << (v[:closed]*100/v[:total]).to_s << " "})
         value = (v[:closed] * 100) / v[:total]
-        leaders[v[:name]] = {label: v[:name], value:  "#{"%02d" % value}%"}
+        leaders[v[:name]] = {label: v[:name], value: value}
       end
     end
   rescue Exception => e
