e54d1b12cace45dc9b5f6b22fb2268b64637063c
MarkKinsman <mark@kinsman.net>
2015-06-26 09:07:13 -0700

    Add sudo to docker command

--- dockerfiles/dashing/docker-restart.sh
+++ dockerfiles/dashing/docker-restart.sh
@@ -11,16 +11,16 @@ git checkout dev
 cd "${WORKDIR}"
 
 # if previous container with same name exists, delete it and build new
-if [[ -n $(docker ps -a | grep -i "field_dashboard" | awk '{print $1}') ]];
+if [[ -n $(sudo docker ps -a | grep -i "field_dashboard" | awk '{print $1}') ]];
 then
     echo -e "\nDeleting previous container named: field_dashboard"
-    docker ps -a | grep -i "field_dashboard" | awk '{print $1}' | xargs docker rm -f
+    sudo docker ps -a | grep -i "field_dashboard" | awk '{print $1}' | xargs docker rm -f
 fi
 
 echo -e "\nBuilding new field_dashboard containter"
-docker build -t markkinsman/dashing .
+sudo docker build -t markkinsman/dashing .
 
-docker run -d -p 80:3030 \
+sudo docker run -d -p 80:3030 \
     --name field_dashboard \
     -e GEMS="rest-client" \
     -v="$WORKDIR"/widgets:/widgets \
@@ -30,6 +30,6 @@ docker run -d -p 80:3030 \
     -v="$WORKDIR"/dashboards:/dashboards \
     markkinsman/dashing
 
-docker ps
+sudo docker ps
 
 echo -e "\nDone!\n"
