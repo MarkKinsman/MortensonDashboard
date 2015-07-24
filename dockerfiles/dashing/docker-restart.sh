161d0648effa940eacc5640a72a627561f4279cf
Marc Kinsman <marc.kinsman@mortenson.com>
2015-07-24 14:37:43 -0400

    Further changes to docker restart

--- dockerfiles/dashing/docker-restart.sh
+++ dockerfiles/dashing/docker-restart.sh
@@ -5,23 +5,51 @@ set -o errexit
 WORKDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 cd "${WORKDIR}"/../../
-git pull origin dev
-git checkout dev
+#git pull origin dev
+#git checkout dev
 
 cd "${WORKDIR}"
 
-# if previous container with same name exists, delete it and build new
-if [[ -n $(docker ps -a | grep -i "field_dashboard" | awk '{print $1}') ]];
-then
-    echo -e "\nDeleting previous container named: field_dashboard"
-    docker ps -a | grep -i "field_dashboard" | awk '{print $1}' | xargs docker rm -f
-fi
+  # if an arguement was passed check if it is a container, if not ask if user wants to create one
+  if [[ $# -ne 0 && ! $(docker ps -a | grep -i $1) ]]; then
+      read -p "The container can not be found, would you like to create a new container? (y/n) "
+      if ! [[ $REPLY =~ ^[Yy]$ ]];
+        then
+        exit 0
+      fi
+      NAME=$1
+  # if argument was found use it
+  elif [[ $# -ne 0 && -n $(docker ps -a | grep -i $1) ]]; then
+    NAME=$1
+    PORT=$(docker port $NAME 3030/tcp | awk --re-interval 'BEGIN{FS=":"}{print $2}')
+  # if no argument was passed, let user choose from list
+  else
+    PS3='Choose a container: '
+    lines=($(docker ps -a | grep 'dashing' | awk --re-interval 'BEGIN{FS="  *"}{ print $(NF-1) }'))
+    select container in "${lines[@]}"
+    do
+      NAME=$container
+      PORT=$(docker port $NAME 3030/tcp | awk --re-interval 'BEGIN{FS=":"}{print $2}')
+      break
+    done
+  fi
 
-echo -e "\nBuilding new field_dashboard containter"
+  if [[ -z "${PORT-}" ]]; then
+    echo -e "Assign a port for the container: \c"
+    read PORT
+  fi
+  # if previous container with same name exists, delete it and build new
+  if [[ -n $(docker ps -a | grep -i $NAME | awk '{ print $1 }') ]]; then
+      echo -e "\nDeleting previous container named: $NAME"
+      docker ps -a | grep -i $NAME | awk '{print $1}' | xargs docker rm -f
+  fi
+
+echo -e "\nBuilding new dashboard containter"
 docker build -t markkinsman/dashing .
 
-docker run -d -p 80:3030 \
-    --name field_dashboard \
+docker run -d -p $PORT:3030 \
+    --name $NAME \
+    -e VIRTUAL_HOST="$NAME".mortenson.systems \
     -e GEMS="rest-client" \
     -v="$WORKDIR"/widgets:/widgets \
     -v="$WORKDIR"/config:/config \
