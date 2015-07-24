9805788f53e0998fe325eb9d9e8daebf5fd3bc41
Marc Kinsman <marc.kinsman@mortenson.com>
2015-07-24 14:35:27 -0400

    Try again

--- dockerfiles/dashing/docker-restart.sh
+++ dockerfiles/dashing/docker-restart.sh
@@ -11,16 +11,17 @@ cd "${WORKDIR}"/../../
 cd "${WORKDIR}"
 
   # if an arguement was passed check if it is a container, if not ask if user wants to create one
-  if [[ $# -ne 0 && ! $(docker ps -a | grep -i $1) ]];
-  then
+  if [[ $# -ne 0 && ! $(docker ps -a | grep -i $1) ]]; then
       read -p "The container can not be found, would you like to create a new container? (y/n) "
       if ! [[ $REPLY =~ ^[Yy]$ ]];
         then
         exit 0
       fi
       NAME=$1
-      echo -e "Enter a port: \c"
-      read PORT
+  # if argument was found use it
+  elif [[ $# -ne 0 && -n $(docker ps -a | grep -i $1) ]]; then
+    NAME=$1
+    PORT=$(docker port $NAME 3030/tcp | awk --re-interval 'BEGIN{FS=":"}{print $2}')
   # if no argument was passed, let user choose from list
   else
     PS3='Choose a container: '
@@ -33,18 +34,22 @@ cd "${WORKDIR}"
     done
   fi
 
+  if [[ -z "${PORT-}" ]]; then
+    echo -e "Assign a port for the container: \c"
+    read PORT
+  fi
   # if previous container with same name exists, delete it and build new
-  if [[ -n $(docker ps -a | grep -i $NAME | awk '{ print $1 }') ]];
-  then
-      echo -e "\nDeleting previous container named: field_dashboard"
+  if [[ -n $(docker ps -a | grep -i $NAME | awk '{ print $1 }') ]]; then
+      echo -e "\nDeleting previous container named: $NAME"
       docker ps -a | grep -i $NAME | awk '{print $1}' | xargs docker rm -f
   fi
 
-echo -e "\nBuilding new field_dashboard containter"
+echo -e "\nBuilding new dashboard containter"
 docker build -t markkinsman/dashing .
 
 docker run -d -p $PORT:3030 \
     --name $NAME \
+    -e VIRTUAL_HOST="$NAME".mortenson.systems \
     -e GEMS="rest-client" \
     -v="$WORKDIR"/widgets:/widgets \
     -v="$WORKDIR"/config:/config \
