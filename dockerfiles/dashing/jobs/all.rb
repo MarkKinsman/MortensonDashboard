fc6bc3fb85ca66bbf1437803de4db85924697b71
MarkKinsman <mark@kinsman.net>
2015-07-28 13:02:55 -0700

    Create Module
    Change the method file to be a module

--- dockerfiles/dashing/jobs/all.rb
+++ dockerfiles/dashing/jobs/all.rb
@@ -1,6 +1,7 @@
 require 'rest-client'
 require 'json'
 require_relative 'bim360helper'
+include Field
 
 #Widgets
 count_widgets=[['all_total'],['all_company_0','all_company_1','all_company_2','all_company_3','all_company_4','all_company_5','all_company_6','all_company_7','all_company_8','all_company_9','all_company_10','all_company_11']]
@@ -10,7 +11,7 @@ debug = ['all_debug', ""]
 login = [0, 0, 0]
 tickets = [0,0]
 leaders = Hash.new({value: 0})
-companies = {total: {:name => 0, :open => 0, :complete => 0, :ready => 0, :closed => 0, :total => 0},companies: Hash.new({name: 0, open: 0, complete: 0, ready: 0, closed: 0, total: 0})}
+companies = {total: {:name => 0, :open => 0, :complete => 0, :ready => 0, :closed => 0, :total => 0}, companies: Hash.new({name: 0, open: 0, complete: 0, ready: 0, closed: 0, total: 0})}
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '10m', :first_in => 0, allow_overlapping: false do |job|
@@ -21,5 +22,5 @@ SCHEDULER.every '10m', :first_in => 0, allow_overlapping: false do |job|
   companies = get_companies(tickets, debug)
   stream = get_issues(tickets, debug)
   companies = issues_company_type_sort(stream, debug)
-  leaders = find_leaders(companies, "all_leaderboard", debug)
-  send_issues(count_widgets, leaders, debug)
+  send_issue_counts(count_widgets, leaders, debug)
+  send_leaders(companies, "all_leaderboard", debug)
--- dockerfiles/dashing/jobs/bim360helper.rb
+++ dockerfiles/dashing/jobs/bim360helper.rb
@@ -1,115 +1,129 @@
-#Reads the file "Login" and gets the corresponding login and project tickets
-#OUT: login[login_ticket, project_ticket]
-#Adds debug code to a debug console textbox
-def get_tickets (debug = "nil")
-  begin
-    File.open(File.expand_path("../login", __FILE__ ), "r") do |rf|
-        login[0] = rf.readline.chomp
-        login[1] = rf.readline.chomp
-        login[2] = rf.readline.chomp
-    end
-  rescue Exception => e
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Open File Error" << e.message << " -> "})
-  else
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Open File Done -> "})
-  end
-  begin
-    stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/login", :params => {:username => username, :password => password}))
-    login_ticket = stream["ticket"]
-    stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/projects", :params => {:ticket => login_ticket}))
-    stream.each do |p|
-      if p["name"] == project
-        project_ticket = p["project_id"]
+module Field
+  #Reads the file "Login" and gets the corresponding login and project tickets
+  #OUT: login[login_ticket, project_ticket]
+  #Adds debug code to a debug console textbox
+  def get_tickets (debug = "nil")
+    begin
+      File.open(File.expand_path("../login", __FILE__ ), "r") do |rf|
+          login[0] = rf.readline.chomp
+          login[1] = rf.readline.chomp
+          login[2] = rf.readline.chomp
       end
+    rescue Exception => e
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Open File Error" << e.message << " -> "})
+    else
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Open File Done -> "})
     end
-  rescue Exception => e
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Unable to get Tickets" << e.message << " -> "})
-  else
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Got Tickets ->"})
-end
-
-#Performs the REST call to the BIM 360 Field Database to recieve companies.
-#IN: Tickets from get_tickets
-#OUT: Hash of company hashes sorted by company_id,
-#Adds debug code to a debug console textbox
-def get_companies (tickets, debug = "nil")
-  begin
-    stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/companies/", :params => {:ticket => tickets[0], :project_id => tickets[1]}))
-    stream.each do |c|
-      companies[:companies][c["company_id"]] = {name: c["name"], open: 0, ready: 0, complete: 0, closed: 0, total: 0}
-    end
-  rescue Exception => e
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Companies Download Error" << e.message << " -> "})
-  else
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Companies Download Done -> "})
-  end
-end
-
-def get_issues (tickets, debug = "nil")
-  begin
-    stream = JSON.parse(RestClient::Request.execute(method: :get, url: "http://bim360field.autodesk.com/api/get_issues/", timeout: nil, headers: {:params => {:ticket => tickets[0], :project_id => tickets[1]}}))
-  rescue Exception => e
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Issues Download Error" << e.message << " -> "})
-  else
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Issues Download Done -> "})
-  end
-end
-
-def issues_company_type_sort (companies, stream, debug = "nil")
-  begin
-    stream.each do |i|
-#      if i["issue_type"].include? "Punch List"
-        case i["status"]
-          when "Open"
-            companies[i[1]["company_id"]][:open] += 1
-            companies[0][:open] += 1
-          when "Work Completed"
-            companies[i[1]["company_id"]][:complete] += 1
-            companies[0][:complete] += 1
-          when "Ready to Inspect"
-            companies[i[1]["company_id"]][:ready] += 1
-            companies[0][:ready] += 1
-          when "Closed"
-            companies[i[1]["company_id"]][:closed] += 1
-            companies[0][:closed] += 1
+    begin
+      stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/login", :params => {:username => username, :password => password}))
+      login_ticket = stream["ticket"]
+      stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/projects", :params => {:ticket => login_ticket}))
+      stream.each do |p|
+        if p["name"] == project
+          project_ticket = p["project_id"]
         end
-        companies[i[1]["company_id"]][:total] += 1
-        companies[0][:total] += 1
-#      end
-    end
-  rescue Exception => e
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Count Issues Error" << e.message << " -> "})
-  else
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Count Issues Done -> " })
-  end
-end
-
-def find_leaders (companies, leaderboard_widget, debug = "nil")
-  begin
-    companies.each do |k, v|
-      if v[:total] != 0
-        value = (v[:closed] * 100) / v[:total]
-        leaders[v[:name]] = {label: v[:name], value: value}
       end
-    end
-    send_event(leaderboard_widget, { items: leaders.values })
-  rescue Exception => e
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Find Leaders Error" << e.message <<  " -> "})
-  else
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Find Leaders Done -> " })
+    rescue Exception => e
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Unable to get Tickets" << e.message << " -> "})
+    else
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Got Tickets ->"})
   end
-end
 
-def send_issues (companies, debug = "nil")
-  begin
-    companies_array = companies.sort_by { |k, v| v[:open] }.reverse!
-    12.times do |i|
-      send_event(widgets[i], {title: companies_array[i][1][:name], open: companies_array[i][1][:open], ready: companies_array[i][1][:ready], complete: companies_array[i][1][:complete], closed: companies_array[i][1][:closed] })
+  #Performs the REST call to the BIM 360 Field Database to recieve companies.
+  #IN: Tickets from get_tickets
+  #OUT: Hash of company hashes sorted by company_id,
+  #Adds debug code to a debug console textbox
+  def get_companies (tickets, debug = "nil")
+    begin
+      stream = JSON.parse(RestClient.get("http://bim360field.autodesk.com/api/companies/", :params => {:ticket => tickets[0], :project_id => tickets[1]}))
+      stream.each do |c|
+        companies[:companies][c["company_id"]] = {name: c["name"], open: 0, ready: 0, complete: 0, closed: 0, total: 0}
+      end
+    rescue Exception => e
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Companies Download Error" << e.message << " -> "})
+    else
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Companies Download Done -> "})
     end
-    send_event(total_widget, {title: "All Issues Total", open: total[:open], closed: total[:closed], ready: total[:ready], complete: total[:complete]})
-  rescue Exception => e
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Display Error" << e.message << " -> "})
-  else
-    unless debug.nil? then send_event(debug[0], {text: debug[1] << "Display Done -> " })
+  end
+
+  #Performs the REST call to the BIM 360 Field Database to recieve issues.
+  #IN: Tickets from get_tickets
+  #OUT: Stream of JSON
+  #Adds debug code to a debug console textbox
+  def get_issues (tickets, debug = "nil")
+    begin
+      stream = JSON.parse(RestClient::Request.execute(method: :get, url: "http://bim360field.autodesk.com/api/get_issues/", timeout: nil, headers: {:params => {:ticket => tickets[0], :project_id => tickets[1]}}))
+    rescue Exception => e
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Issues Download Error" << e.message << " -> "})
+    else
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Issues Download Done -> "})
+    end
+  end
+
+  #Increments the company issue counts base don type of issues
+  #IN: Companies Hash, JSON Stream of issues
+  #OUT: Hash of company hashes sorted by company_id with counted issues
+  #Adds debug code to a debug console textbox
+  def issues_company_type_sort (companies, stream, debug = "nil")
+    begin
+      stream.each do |i|
+          case i["status"]
+            when "Open"
+              companies[i[:companies]["company_id"]][:open] += 1
+              companies[:total][:open] += 1
+            when "Work Completed"
+              companies[i[:companies]["company_id"]][:complete] += 1
+              companies[:total][:complete] += 1
+            when "Ready to Inspect"
+              companies[i[:companies]["company_id"]][:ready] += 1
+              companies[:total][:ready] += 1
+            when "Closed"
+              companies[i[:companies]["company_id"]][:closed] += 1
+              companies[:total][:closed] += 1
+          end
+          companies[i[:companies]["company_id"]][:total] += 1
+          companies[:total][:total] += 1
+      end
+    rescue Exception => e
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Count Issues Error" << e.message << " -> "})
+    else
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Count Issues Done -> " })
+    end
+  end
+
+  #Calculates the %Complete for companies and displays them in the leaderboard
+  #IN: Companies hash, Text name of leaderboard widget
+  #Adds debug code to a debug console textbox
+  def send_leaders (companies, leaderboard_widget, debug = "nil")
+    begin
+      companies[:companies].each do |k, v|
+        if v[:total] != 0
+          value = (v[:closed] * 100) / v[:total]
+          leaders[v[:name]] = {label: v[:name], value: value}
+        end
+      end
+      send_event(leaderboard_widget, { items: leaders.values })
+    rescue Exception => e
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Find Leaders Error" << e.message <<  " -> "})
+    else
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Find Leaders Done -> " })
+    end
+  end
+
+  #Orders companies based on open issues and dislpays the ones with the most in the widgets
+  #IN: Companies hash, Array of text names for widgets
+  #Adds debug code to a debug console textbox
+  def send_issue_counts (companies, widgets, debug = "nil")
+    begin
+      companies_array = companies[:companies].sort_by { |k, v| v[:open] }.reverse!
+      12.times do |i|
+        send_event(widgets[1][i], {title: companies_array[i][1][:name], open: companies_array[i][1][:open], ready: companies_array[i][1][:ready], complete: companies_array[i][1][:complete], closed: companies_array[i][1][:closed] })
+      end
+      send_event(widgets[1][0], {title: "All Issues Total", open: companies[:total][:open], closed: companies[:total][:closed], ready: companies[:total][:ready], complete: companies[:total][:complete]})
+    rescue Exception => e
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Display Error" << e.message << " -> "})
+    else
+      unless debug.nil? then send_event(debug[0], {text: debug[1] << "Display Done -> " })
+    end
   end
 end
