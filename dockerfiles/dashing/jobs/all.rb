5b22c5de4c5c2bb19145f3cf0cef47472402ba32
MarkKinsman <mark@kinsman.net>
2015-07-29 15:16:55 -0700

    Revert "Push to test"
    This reverts commit 01f3011596d139717458e5f912046fd6e5f30e9a.

--- dockerfiles/dashing/jobs/all.rb
+++ dockerfiles/dashing/jobs/all.rb
@@ -10,9 +10,9 @@ require 'json'
     login = {:username => 0 , :password => 0, :project => 0 }
     tickets = {:login => 0, :project => 0}
     File.open(File.expand_path("../login", __FILE__ ), "r") do |rf|
-        login[0] = rf.readline.chomp
-        login[1] = rf.readline.chomp
-        login[2] = rf.readline.chomp
+        login[:username] = rf.readline.chomp
+        login[:password] = rf.readline.chomp
+        login[:project] = rf.readline.chomp
     end
 
     send_event("all_debug", {text: login[:username] << " - " << login[:password] << " - " << login[:project]})
