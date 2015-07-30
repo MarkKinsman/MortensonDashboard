aed5efbf011ae6339e5025fcae90ab720eb75724
MarkKinsman <mark@kinsman.net>
2015-07-30 00:39:45 -0700

    Forgot to save last

--- dockerfiles/dashing/jobs/all.rb
+++ dockerfiles/dashing/jobs/all.rb
@@ -5,7 +5,7 @@ require_relative 'bim360helper'
 tickets = {:login => 0, :project => 0}
 
 #Widgets
-count_widgets=[['all_total'],['all_company_0','all_company_1','all_company_2','all_company_3','all_company_4','all_company_5','all_company_6','all_company_7','all_company_8','all_company_9','all_company_10','all_company_11']]
+count_widgets=['all_total', 'all_company_0','all_company_1','all_company_2','all_company_3','all_company_4','all_company_5','all_company_6','all_company_7','all_company_8','all_company_9','all_company_10','all_company_11']]
 debug = ['all_debug', ""]
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
@@ -38,7 +38,7 @@ SCHEDULER.every '10m', :first_in => 0, allow_overlapping: false do |job|
   end
 
   begin
-    companies = Field.issues_company_type_sort(companies, issues_stream)
+    companies,total = Field.issues_company_type_count(companies, issues_stream)
   rescue Exception => e
     unless debug.nil? then send_event(debug[0], {text: debug[1] << "Count Issues Error" + e.message + " -> "}) end
   else
