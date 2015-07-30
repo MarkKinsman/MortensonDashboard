8a159c44e8653192ba774111060e0d65479a4d34
MarkKinsman <mark@kinsman.net>
2015-07-30 11:03:32 -0700

    Push to test

--- dockerfiles/dashing/jobs/bim360helper.rb
+++ dockerfiles/dashing/jobs/bim360helper.rb
@@ -72,16 +72,16 @@ module Field
   #Orders companies based on open issues and dislpays the ones with the most in the widgets
   #IN: Companies hash, Array of text names for widgets
   def self.send_issue_counts (companies, widgets, total=nil)
-    companies_array = companies.sort_by { |v| v[:open] }
+    companies_array = companies.sort_by { |k, v| v[:open] }
     unless total == nil
-      companies_array.unshift(total)
+      companies_array.unshift([0, total])
     end
 
     send_event("all_debug", {text: companies_array.inspect })
     sleep(200)
 
     widgets.length.times do |i|
-      send_event(widgets[i], {title: companies_array[i][:name], open: companies_array[i][:open], ready: companies_array[i][:ready], complete: companies_array[i][:complete], closed: companies_array[i][:closed] })
+      send_event(widgets[i], {title: companies_array[i][1][:name], open: companies_array[i][1][:open], ready: companies_array[i][1][:ready], complete: companies_array[i][1][:complete], closed: companies_array[i][1][:closed] })
     end
   end
 
