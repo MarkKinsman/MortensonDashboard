30b75983b3c81b6f3eddceca0c73923b842e1fad
MarkKinsman <mark@kinsman.net>
2015-08-05 14:20:28 -0700

    Push to test

--- dockerfiles/dashing/jobs/all.rb
+++ dockerfiles/dashing/jobs/all.rb
@@ -42,7 +42,7 @@ SCHEDULER.every '10m', :first_in => 0, allow_overlapping: false do |job|
 
     (issues_count/20).times do |i|
       stream = Field.get_issues(tickets, 20, i)
-      punch_stream = stream.select{ |k| k.has_key?("issue_type") && k["issue_type"].include? "Punch List"}
+      punch_stream = stream.select { |k,v| k.has_key?("issue_type") && k["issue_type"].include?("Punch List")}
       all_companies, all_total = Field.company_issue_count(all_companies, stream, all_total)
       punch_companies, punch_total = Field.company_issue_count(punch_companies, punch_stream, punch_total)
       send_event(debug[0], {text: debug[1] << (100*iterator)/(issues_count/20)})
